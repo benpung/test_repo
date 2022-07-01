1adde2073e139b1cb4a3c6d3e0bdc45474b8ac82
Ben Pung <pung_benjamin_d@lilly.com>
2022-07-01 10:26:07 -0400

    fixed bugs

--- encode_decode.py
+++ encode_decode.py
@@ -9,8 +9,8 @@ def encode_string(i):
     encode a string using base64
     '''
     print('encode input value: ' + i)
-    encoded_value = base64.b64encode(b,i)
-    print('base64 encoded value in bytes: ' + encoded_value)
+    encoded_value = base64.b64encode(i.encode())
+    print('base64 encoded value in bytes: ' + str(encoded_value))
 
 
 def decode_string(i):
@@ -18,21 +18,21 @@ def decode_string(i):
     decode a string using base64
     '''
     print('decode input value: ' + i)
-    encoded_value = base64.b64decode(i).decode('utf-8')
-    print('base64 decoded value in utf-8: ' + encoded_value)
+    decoded_value = base64.b64decode(i).decode('utf-8')
+    print('base64 decoded value in utf-8: ' + str(decoded_value))
 
 print('starting encode/decode utility')
-print('enter E to encode or D to decode:')
+print('enter e to encode or d to decode:')
 i_mode = input()
 
-if i_mode == 'E':
+if i_mode == 'e':
     print('enter string to encode:')
     i_encode = input()
     encode_string(i_encode)
-elif i_mode == 'D':
+elif i_mode == 'd':
     print('enter string to decode:')
     i_decode = input()
     decode_string(i_decode)
 else:
-    print('invalid input.  enter E or D.')
+    print('invalid input.  enter e or d.  exiting.')
     exit
