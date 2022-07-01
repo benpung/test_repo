cc055c031f6b1a20177e04da06a3eae25c6cc5ec
Ben Pung <pung_benjamin_d@lilly.com>
2022-07-01 09:38:05 -0400

    added calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def addition(i,i1,i2):
+    '''
+    this function adds the input numbers
+    '''
+    total = 1+i1+i2
+    print('sum of ' + str(i) + ' and ' + str(i2) + ' and ' + str(i2) + ' is: ' + str(total))
+
+
+def subtraction():
+    '''
+    this function adds the input numbers
+    '''
+    print('subraction successful')
+
+print('starting code')
+addition(1,2,3)
+subtraction()
